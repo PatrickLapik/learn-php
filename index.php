139343a697f8ee817e34df4062ee0e9800ce133b
PatrickLapik <patrick.lapimaa@tptlive.ee>
2024-09-10 10:52:33 +0300

    magick methods

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
 
-$i = 1;
+class Box {
+  public $var;
+  public $foo;
+  public function __construct($var, $foo) {
+    var_dump($var, $foo);
 
-var_dump($i);
+    $this->var = $var;
+    $this->foo = $foo;
+  }
+  public function __set($name, $value) {
+    var_dump($name, $value);
+  }
+  public function __get($name) {
+    return "property $name does not exist";
+  }
+  public function __call($name, $args) {
+    var_dump($name, $args);
+  }
+  public function __destruct() {
+    echo 'box was destroyed :"(';
+  }
+}
 
-$array = [
-  'num' => 1,
-  'pp' => 2,
-];
+$box = new Box('dr', 'vitt');
 
-var_dump($array['pp']);
+$box->bar = 'asd;k';
+var_dump($box->ks);
+
+$box->hello(1,2,3,4,4);
+
+var_dump(serialize($box));
