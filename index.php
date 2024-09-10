1c84dc48541850c2d42b4d8312ee0b2c5a81ab4a
PatrickLapik <patrick.lapimaa@tptlive.ee>
2024-09-10 11:06:13 +0300

    static functions and properties

--- index.php
+++ index.php
@@ -1,33 +1,17 @@
 <?php
 
 class Box {
-  public $var;
-  public $foo;
-  public function __construct($var, $foo) {
-    var_dump($var, $foo);
+  public static $count = 0;
 
-    $this->var = $var;
-    $this->foo = $foo;
-  }
-  public function __set($name, $value) {
-    var_dump($name, $value);
-  }
-  public function __get($name) {
-    return "property $name does not exist";
-  }
-  public function __call($name, $args) {
-    var_dump($name, $args);
-  }
-  public function __destruct() {
-    echo 'box was destroyed :"(';
+  public static function test() {
+    var_dump(static::class);
   }
 }
 
-$box = new Box('dr', 'vitt');
+class MetalBox extends Box {
 
-$box->bar = 'asd;k';
-var_dump($box->ks);
+}
 
-$box->hello(1,2,3,4,4);
-
-var_dump(serialize($box));
+Box::$count = 1;
+MetalBox::test();
+var_dump(Box::$count);
