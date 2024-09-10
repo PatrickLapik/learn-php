4cc28c24d9002e990b53f3b81482bf79eadfd803
PatrickLapik <patrick.lapimaa@tptlive.ee>
2024-09-10 11:40:30 +0300

    php page

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
-<?php
+<?php include 'partials/header.php' ?>
 
-class Box {
-  public static $count = 0;
+<!doctype html>
 
-  public static function test() {
-    var_dump(static::class);
-  }
-}
+<html lang="en">
 
-class MetalBox extends Box {
+<body>
 
-}
 
-Box::$count = 1;
-MetalBox::test();
-var_dump(Box::$count);
+
+  <main class="container">
+
+    <?php include 'partials/hero.php' ?>
+
+    <?php include 'partials/featured.php' ?>
+
+    <div class="row g-5">
+      <div class="col-md-8">
+        <h3 class="pb-4 mb-4 fst-italic border-bottom">
+          From the Firehose
+        </h3>
+
+
+        <?php include 'partials/posts.php' ?>
+
+        <nav class="blog-pagination" aria-label="Pagination">
+          <a class="btn btn-outline-primary" href="#">Older</a>
+          <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+        </nav>
+
+      </div>
+
+
+      <?php include 'partials/nav.php' ?>
+
+  </main>
+
+  <?php include 'partials/footer.php' ?>
+
+
+
+</body>
+
+</html>
